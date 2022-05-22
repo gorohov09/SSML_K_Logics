3c82c80900183b661eb22db079eeb3c59788d079
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-22 23:15:17 +0300

    Класс, предназначенный для проверки корректности ввода данных

--- Services/SSML_K_Logics.K_DigitLogic/StringHelper.cs
+++ Services/SSML_K_Logics.K_DigitLogic/StringHelper.cs
@@ -15,9 +15,35 @@ namespace SSML_K_Logics.K_DigitLogic
             _stringExpression = stringExpression;
         }
 
-        public void Working()
+        public bool IsCorrect(int k) => CheckBrackets() && CheckOperation() && CheckIndex(k);
+
+        private bool CheckBrackets()
         {
-            string[] subExpr = _stringExpression.Split('v');
+            return _stringExpression.Count(ch => ch == '(' || ch == ')') % 2 == 0;
+        }
+
+        private bool CheckOperation()
+        {
+            for (int i = 0; i < _stringExpression.Length; i++)
+            {
+                if (_stringExpression[i] == 'v' && _stringExpression[i] == 'v')
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckIndex(int k)
+        {
+            for (int i = 0; i < _stringExpression.Length; i++)
+            {
+                if (_stringExpression[i] == 'j')
+                {
+                    int number = int.Parse(Convert.ToString(_stringExpression[i + 2]));
+                    if (number >= k)
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
