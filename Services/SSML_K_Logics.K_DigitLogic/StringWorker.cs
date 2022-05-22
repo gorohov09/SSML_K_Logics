1c8083c29323f59ba932d13864cc1c5f6826d61e
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-22 19:04:39 +0300

    Заготовка класса, для работы со строкой

---
+++ Services/SSML_K_Logics.K_DigitLogic/StringWorker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSML_K_Logics.K_DigitLogic
+{
+    public class StringWorker
+    {
+        private readonly string _stringExpression;
+
+        public StringWorker(string stringExpression)
+        {
+            _stringExpression = stringExpression;
+        }
+
+        public void Working()
+        {
+            string[] subExpr = _stringExpression.Split('v');
+        }
+    }
+}
