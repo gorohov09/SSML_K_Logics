fdde201fae1f94fd1192d67d2b9e866ad37db0de
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-22 16:40:07 +0300

    Создание характеристической функции первого рода

---
+++ Services/SSML_K_Logics.K_DigitLogic/Function/FirstCharacteristicFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSML_K_Logics.K_DigitLogic.Function
+{
+    public class FirstCharacteristicFunction
+    {
+        private readonly int _i;
+
+        public FirstCharacteristicFunction(int i) { this._i = i; }
+
+        public int Result(int x) => x == _i ? 1 : 0;    
+    }
+}
