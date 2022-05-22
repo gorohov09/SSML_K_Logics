f5444351026fc4b02e9d5dde4a474c2e27cd228c
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-22 20:03:06 +0300

    Добавление возможности вводить приоритет операций - скобками

--- UI/SSML_K_Logics.ConsoleApp/Program.cs
+++ UI/SSML_K_Logics.ConsoleApp/Program.cs
@@ -11,7 +11,7 @@ namespace SSML_K_Logics.App.ConsoleApp
             Disjunction dis = new Disjunction();
             FirstCharacteristicFunction func = new FirstCharacteristicFunction();
             Calculation calc = new Calculation(3, 2, dis, func);
-            calc.Calculate("x");
+            calc.Calculate("(j_1[x]v((j_2[y]vx)vy))");
         }
     }
 }
