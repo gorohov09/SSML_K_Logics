ecb678ce133a08dcccfb7b3e43c7ef87f5f78217
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-22 16:57:57 +0300

    Создание класса, отвечающего за обобщенную дизъюнкцию

---
+++ Services/SSML_K_Logics.K_DigitLogic/Function/Disjunction.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SSML_K_Logics.K_DigitLogic.Function
+{
+    public class Disjunction
+    {
+        public int Result(int x, int y) => Math.Max(x, y);
+    }
+}
--- Services/SSML_K_Logics.K_DigitLogic/Function/FirstCharacteristicFunction.cs
+++ Services/SSML_K_Logics.K_DigitLogic/Function/FirstCharacteristicFunction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SSML_K_Logics.K_DigitLogic.Function
+﻿namespace SSML_K_Logics.K_DigitLogic.Function
 {
     public class FirstCharacteristicFunction
     {
