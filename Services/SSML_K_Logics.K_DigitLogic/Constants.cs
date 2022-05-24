ef5312e1edad3111a605100e45e43936bca53edc
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-24 17:54:25 +0300

    Добавил константы

---
+++ Services/SSML_K_Logics.K_DigitLogic/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSML_K_Logics.K_DigitLogic
+{
+    public class Constants
+    {
+        public const string PATH_FILE_TXT = "data.txt";
+    }
+}
